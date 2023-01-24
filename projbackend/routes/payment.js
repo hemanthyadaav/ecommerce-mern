db057740449c5503fc8bb161b2c63e7d7b1f8363
U Hemantha Kumar <hemanthkumaryadav2001@gmail.com>
2023-01-25 01:36:49 +0530

    route param method called in payment route file

--- projbackend/routes/payment.js
+++ projbackend/routes/payment.js
@@ -3,8 +3,18 @@ const router = express.Router();
 
 const { isSignedIn, isAuthorized } = require("../controllers/authentication");
 const { getToken, processPayment } = require("../controllers/payment");
+const { getUserById } = require("../controllers/user");
 
-router.get("/payment/gettoken/:userId", isSignedIn, getToken);
-router.post("/payment/braintree/:userId", isSignedIn, processPayment);
+// params
+router.param("userId", getUserById);
+
+// routes
+router.get("/payment/gettoken/:userId", isSignedIn, isAuthorized, getToken);
+router.post(
+  "/payment/braintree/:userId",
+  isSignedIn,
+  isAuthorized,
+  processPayment
+);
 
 module.exports = router;
